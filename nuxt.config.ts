450eaf25da893577dfa4dbddf90bc10e486a9ec0
colingue <colin.noiret@gmail.com>
2025-02-16 15:36:53 +0100

    Change icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@pinia/nuxt"],
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/svg+xml", href: "/budget2travel.svg" },
+      ],
+    },
+  },
 });
